8e72bdc8bbf86d076f136f0821e5db8bdba1e073
Artem <artsiom.menshakou@p97.com>
2021-10-10 16:42:40 +0200

    [MODULE-1/TASK-02] create calculator app

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,42 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const AppRoot = defineComponent({
+  data: () => ({
+    firstValue: 0,
+    secondValue: 0,
+    radioValue: 'sum',
+  }),
+
+  computed: {
+    calculated() {
+      const value = this._calculateValue(this.radioValue, +this.firstValue, +this.secondValue);
+
+      if (!isNaN(value)) {
+        return value;
+      } else {
+        return 'Mission impossible';
+      }
+    },
+  },
+
+  methods: {
+    _calculateValue: function (value, first, second) {
+      switch (value) {
+        case 'sum':
+          return first + second;
+        case 'subtract':
+          return first - second;
+        case 'multiply':
+          return first * second;
+        case 'divide':
+          return first / second;
+      }
+    },
+  },
+});
+
+const app = createApp(AppRoot);
+
+const vm = app.mount('#app');
