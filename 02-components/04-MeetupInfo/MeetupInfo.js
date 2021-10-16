4fa6b72109497ffd19c9d3e995eef973d884be51
Artem <artsiom.menshakou@p97.com>
2021-10-16 13:36:28 +0200

    [MODULE-2] update component meetup info

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -2,20 +2,50 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'MeetupInfo',
+  props: {
+    organizer: {
+      type: String,
+      required: true,
+    },
+    place: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  computed: {
+    localizeDate() {
+      const vm = this;
+
+      return function (isIsoFormat) {
+        if (!isIsoFormat) {
+          return new Date(vm.date).toLocaleDateString(navigator.language, {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          });
+        } else return new Date(vm.date).toISOString().substr(0, 10);
+      };
+    },
+  },
 
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="localizeDate(true)">{{ localizeDate() }}</time>
       </li>
     </ul>`,
 });
